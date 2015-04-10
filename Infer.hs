e647ad8eb5005dfd37732413f3c71bb14c1a4ee8
Olle Fredriksson <fredriksson.olle@gmail.com>
2015-04-10 10:51:23 +0200

    Move subtype to Unify.hs

--- Infer.hs
+++ Infer.hs
@@ -7,7 +7,6 @@ import Control.Monad.ST()
 import Data.Bitraversable
 import Data.Foldable as F
 import qualified Data.Map as M
-import Data.Monoid
 import qualified Data.Set as S
 import Text.Trifecta.Result
 
@@ -21,6 +20,76 @@ import Pretty
 import TopoSort
 import Unify
 import Util
+
+checkType :: Input s -> Core s -> TCM s (Core s, Core s)
+checkType expr typ = do
+  tr "checkType e" expr
+  tr "          t" =<< freeze typ
+  modifyIndent succ
+  (rese, rest) <- case expr of
+    Input.Lam m p s -> do
+      typ' <- whnf typ
+      case typ' of
+        Core.Pi h p' a ts | p == p' -> do
+          v <- freshForall h a
+          (body, ts') <- checkType (instantiate1 (return v) s)
+                                   (instantiate1 (return v) ts)
+          expr' <- Core.Lam m p a <$> abstract1M v body
+          typ'' <- Core.Pi m p a <$> abstract1M v ts'
+          return (expr', typ'')
+        _ -> inferIt
+    _ -> inferIt
+  modifyIndent pred
+  tr "checkType res e" rese
+  tr "              t" rest
+  return (rese, rest)
+    where
+      inferIt = do
+        (expr', typ') <- inferType expr
+        subtype expr' typ' typ
+
+inferType :: Input s -> TCM s (Core s, Core s)
+inferType expr = do
+  tr "inferType" expr
+  modifyIndent succ
+  (e, t) <- case expr of
+    Input.Var v     -> return (Core.Var v, metaType v)
+    Input.Type      -> return (Core.Type, Core.Type)
+    Input.Pi n p Nothing s -> do
+      tv  <- freshExistsV mempty Core.Type
+      v   <- freshForall n tv
+      (e', _)  <- checkType (instantiate1 (return v) s) Core.Type
+      s'  <- abstract1M v e'
+      return (Core.Pi n p tv s', Core.Type)
+    Input.Pi n p (Just t) s -> do
+      (t', _) <- checkType t Core.Type
+      v  <- freshForall n t'
+      (e', _) <- checkType (instantiate1 (return v) s) Core.Type
+      s' <- abstract1M v e'
+      return (Core.Pi n p t' s', Core.Type)
+    Input.Lam n p s -> uncurry generalise <=< enterLevel $ do
+      a   <- freshExistsV mempty Core.Type
+      b   <- freshExistsV mempty Core.Type
+      x   <- freshForall n a
+      (e', b')  <- checkType (instantiate1 (return x) s) b
+      s'  <- abstract1M x e'
+      ab  <- abstract1M x b'
+      return (Core.Lam n p a s', Core.Pi n p a ab)
+    Input.App e1 p e2 -> do
+      (e1', vt, s) <- inferPi e1 p
+      (e2', _) <- checkType e2 vt
+      return (Core.App e1' p e2', instantiate1 e2' s)
+    Input.Anno e t  -> do
+      (t', _) <- checkType t Core.Type
+      checkType e t'
+    Input.Wildcard  -> do
+      t <- freshExistsV mempty Core.Type
+      x <- freshExistsV mempty t
+      return (x, t)
+  modifyIndent pred
+  tr "inferType res e" e
+  tr "              t" t
+  return (e, t)
 
 inferPi :: Input s -> Plicitness
         -> TCM s (Core s, Core s, Scope1 Core.Expr (MetaVar s))
@@ -74,8 +143,6 @@ generalise expr typ = do
     return (x, ds)
    )
   let sorted = map go $ topoSort deps
-  -- fexpr <- freeze expr
-  -- ftyp  <- freeze typ
   genexpr <- F.foldrM ($ Core.etaLam) expr sorted
   gentype <- F.foldrM ($ Core.Pi)     typ  sorted
 
@@ -86,150 +153,6 @@ generalise expr typ = do
   where
     go [a] f = fmap (f (metaHint a) Implicit $ metaType a) . abstract1M a
     go _   _ = error "Generalise"
-
-inferType :: Input s -> TCM s (Core s, Core s)
-inferType expr = do
-  tr "inferType" expr
-  modifyIndent succ
-  (e, t) <- case expr of
-    Input.Var v     -> return (Core.Var v, metaType v)
-    Input.Type      -> return (Core.Type, Core.Type)
-    Input.Pi n p Nothing s -> do
-      tv  <- freshExistsV mempty Core.Type
-      v   <- freshForall n tv
-      (e', _)  <- checkType (instantiate1 (return v) s) Core.Type
-      s'  <- abstract1M v e'
-      return (Core.Pi n p tv s', Core.Type)
-    Input.Pi n p (Just t) s -> do
-      (t', _) <- checkType t Core.Type
-      v  <- freshForall n t'
-      (e', _) <- checkType (instantiate1 (return v) s) Core.Type
-      s' <- abstract1M v e'
-      return (Core.Pi n p t' s', Core.Type)
-    Input.Lam n p s -> uncurry generalise <=< enterLevel $ do
-      a   <- freshExistsV mempty Core.Type
-      b   <- freshExistsV mempty Core.Type
-      x   <- freshForall n a
-      (e', b')  <- checkType (instantiate1 (return x) s) b
-      s'  <- abstract1M x e'
-      ab  <- abstract1M x b'
-      return (Core.Lam n p a s', Core.Pi n p a ab)
-    Input.App e1 p e2 -> do
-      (e1', vt, s) <- inferPi e1 p
-      (e2', _) <- checkType e2 vt
-      return (Core.App e1' p e2', instantiate1 e2' s)
-    Input.Anno e t  -> do
-      (t', _) <- checkType t Core.Type
-      checkType e t'
-    Input.Wildcard  -> do
-      t <- freshExistsV mempty Core.Type
-      x <- freshExistsV mempty t
-      return (x, t)
-  modifyIndent pred
-  tr "inferType res e" e
-  tr "              t" t
-  return (e, t)
-
-subtype :: Core s -> Core s -> Core s -> TCM s (Core s, Core s)
-subtype expr type1 type2 = do
-  tr "subtype e"  expr
-  tr "        t1" type1
-  tr "        t2" type2
-  modifyIndent succ
-  (e', type') <- go True expr type1 type2
-  modifyIndent pred
-  tr "subtype res e'" e'
-  tr "            type'" type'
-  return (e', type')
-  where
-    go reduce e typ1 typ2
-      | typ1 == typ2 = return (e, typ2)
-      | otherwise = case (typ1, typ2) of
-        (Core.Pi h1 p1 t1 s1, Core.Pi h2 p2 t2 s2) | p1 == p2 -> do
-          let h = h1 <> h2
-          x2  <- freshForall h t2
-          (x1, _)   <- subtype (return x2) t2 t1
-          (ex, s2') <- subtype (Core.App e p1 x1)
-                                (instantiate1 x1 s1)
-                                (instantiate1 x1 s2)
-          e2    <- Core.etaLam h p1 t2 <$> abstract1M x2 ex
-          typ2' <- Core.Pi h p1 t2 <$> abstract1M x2 s2'
-          return (e2, typ2')
-        {-
-        (Core.Pi n p t1 s1, Core.Var v@(metaRef -> Just r)) -> do
-          sol <- solution r
-          case sol of
-            Left l -> do
-              occurs l v typ1
-              unify (metaType v) Core.Type
-              t2  <- freshExistsLV Core.Type l
-              t2' <- freshExistsLV Core.Type l
-              x2  <- freshExists t2
-              solve r . Core.Pi n p t2 =<< abstract1M x2 t2'
-              x1  <- subtype (return x2) t2 t1
-              ex  <- subtype (Core.App e p x1) (instantiate1 x1 s1) t2'
-              refineSolved r . Core.Pi n p t2 =<< abstract1M x2 t2'
-              Core.etaLam n p t2 <$> abstract1M x2 ex
-            Right c -> subtype e typ1 c
-        -}
-        (Core.Var v@(metaRef -> Just r), Core.Pi h p t2 s2) -> do
-          sol <- solution r
-          case sol of
-            Left l -> do
-              occurs l v typ2
-              unify (metaType v) Core.Type
-              t11  <- freshExistsLV (metaHint v) Core.Type l
-              t12 <- freshExistsLV (metaHint v) Core.Type l
-              solve r $ Core.Pi h p t11 $ abstractNothing t12
-              x2  <- freshForall h t2
-              (x1, t11') <- subtype (return x2) t2 t11
-              (ex, s2')  <- subtype (Core.betaApp e p x1) t12 (instantiate1 (return x2) s2)
-              refineSolved r . Core.Pi h p t11' =<< abstract1M x2 s2'
-              e2    <- Core.etaLam h p t2 <$> abstract1M x2 ex
-              typ2' <- Core.Pi h p t2 <$> abstract1M x2 s2'
-              return (e2, typ2')
-            Right c -> subtype e c typ2
-        (_, Core.Pi h Implicit t2 s2) -> do
-          x2 <- freshForall h t2
-          (e2, s2') <- subtype e typ1 (instantiate1 (return x2) s2)
-          e2'   <- Core.etaLam h Implicit t2 <$> abstract1M x2 e2
-          typ2' <- Core.Pi     h Implicit t2 <$> abstract1M x2 s2'
-          return (e2', typ2')
-        (Core.Pi h Implicit t1 s1, _) -> do
-          v1 <- freshExistsV h t1
-          subtype (Core.betaApp e Implicit v1) (instantiate1 v1 s1) typ2
-        _ | reduce -> do
-          typ1' <- whnf typ1
-          typ2' <- whnf typ2
-          go False e typ1' typ2'
-        _ -> do unify typ1 typ2; return (e, typ2)
-
-checkType :: Input s -> Core s -> TCM s (Core s, Core s)
-checkType expr typ = do
-  tr "checkType e" expr
-  tr "          t" =<< freeze typ
-  modifyIndent succ
-  (rese, rest) <- case expr of
-    Input.Lam m p s -> do
-      typ' <- whnf typ
-      case typ' of
-        Core.Pi h p' a ts | p == p' -> do
-          v <- freshForall h a
-          (body, ts') <- checkType (instantiate1 (return v) s)
-                                   (instantiate1 (return v) ts)
-          expr' <- Core.Lam m p a <$> abstract1M v body
-          typ'' <- Core.Pi m p a <$> abstract1M v ts'
-          return (expr', typ'')
-        _ -> inferIt
-    _ -> inferIt
-  modifyIndent pred
-  tr "checkType res e" rese
-  tr "              t" rest
-  return (rese, rest)
-    where
-      inferIt = do
-        (expr', typ') <- inferType expr
-        subtype expr' typ' typ
 
 data Empty
 fromEmpty :: Empty -> a
--- Unify.hs
+++ Unify.hs
@@ -6,6 +6,7 @@ import Control.Monad.Except
 import Control.Monad.ST.Class
 import Data.Bifunctor
 import Data.Foldable
+import Data.Monoid
 import qualified Data.Set as S
 import Data.STRef
 
@@ -39,7 +40,7 @@ unify type1 type2 = do
     go reduce t1 t2
       | t1 == t2  = return ()
       | otherwise = case (t1, t2) of
-        (Core.Var v1@(metaRef -> Just r1), Core.Var v2@(metaRef -> Just r2)) -> do
+        (Var v1@(metaRef -> Just r1), Var v2@(metaRef -> Just r2)) -> do
           unify (metaType v1) (metaType v2)
           sol1 <- solution r1
           sol2 <- solution r2
@@ -53,17 +54,17 @@ unify type1 type2 = do
         -- are distinct universally quantified variables, then 'f = \xs. t' is
         -- a most general solution (See Miller, Dale (1991) "A Logic
         -- programming...")
-        (Core.appsView -> (Core.Var v@(metaRef -> Just r), distinctForalls -> Just pvs), _) -> solveVar r v pvs t2
-        (_, Core.appsView -> (Core.Var v@(metaRef -> Just r), distinctForalls -> Just pvs)) -> solveVar r v pvs t1
-        (Core.Pi  h p1 a s1, Core.Pi  _ p2 b s2) | p1 == p2 -> absCase h a b s1 s2
-        (Core.Lam h p1 a s1, Core.Lam _ p2 b s2) | p1 == p2 -> absCase h a b s1 s2
+        (appsView -> (Var v@(metaRef -> Just r), distinctForalls -> Just pvs), _) -> solveVar r v pvs t2
+        (_, appsView -> (Var v@(metaRef -> Just r), distinctForalls -> Just pvs)) -> solveVar r v pvs t1
+        (Pi  h p1 a s1, Pi  _ p2 b s2) | p1 == p2 -> absCase h a b s1 s2
+        (Lam h p1 a s1, Lam _ p2 b s2) | p1 == p2 -> absCase h a b s1 s2
 
         -- If we've already tried reducing the application,
         -- we can only hope to unify it pointwise.
-        (Core.App e1 p1 e1', Core.App e2 p2 e2') | p1 == p2 && not reduce -> do
+        (App e1 p1 e1', App e2 p2 e2') | p1 == p2 && not reduce -> do
           go True e1  e2
           go True e1' e2'
-        (Core.Type, Core.Type) -> return ()
+        (Type, Type) -> return ()
         _ | reduce             -> do
           t1' <- whnf t1
           t2' <- whnf t2
@@ -75,7 +76,7 @@ unify type1 type2 = do
       go True (instantiate1 (return v) s1) (instantiate1 (return v) s2)
     distinctForalls pes | distinct pes = traverse isForall pes
                         | otherwise    = Nothing
-    isForall (p, Core.Var v@(metaRef -> Nothing)) = Just (p, v)
+    isForall (p, Var v@(metaRef -> Nothing)) = Just (p, v)
     isForall _                                    = Nothing
     distinct pes = S.size (S.fromList es) == length es where es = map snd pes
     solveVar r v pvs t = do
@@ -84,5 +85,62 @@ unify type1 type2 = do
         Left l  -> do
           occurs l v t
           solve r =<< lams pvs t
-        Right c -> go True (Core.apps c (map (second return) pvs)) t
-    lams pvs t = foldrM (\(p, v) -> fmap (Core.Lam (Hint Nothing) p $ metaType v) . abstract1M v) t pvs
+        Right c -> go True (apps c (map (second return) pvs)) t
+    lams pvs t = foldrM (\(p, v) -> fmap (Lam (Hint Nothing) p $ metaType v) . abstract1M v) t pvs
+
+subtype :: Core s -> Core s -> Core s -> TCM s (Core s, Core s)
+subtype expr type1 type2 = do
+  tr "subtype e"  expr
+  tr "        t1" type1
+  tr "        t2" type2
+  modifyIndent succ
+  (e', type') <- go True expr type1 type2
+  modifyIndent pred
+  tr "subtype res e'" e'
+  tr "            type'" type'
+  return (e', type')
+  where
+    go reduce e typ1 typ2
+      | typ1 == typ2 = return (e, typ2)
+      | otherwise = case (typ1, typ2) of
+        (Pi h1 p1 t1 s1, Pi h2 p2 t2 s2) | p1 == p2 -> do
+          let h = h1 <> h2
+          x2  <- freshForall h t2
+          (x1, _)   <- subtype (return x2) t2 t1
+          (ex, s2') <- subtype (App e p1 x1)
+                                (instantiate1 x1 s1)
+                                (instantiate1 x1 s2)
+          e2    <- etaLam h p1 t2 <$> abstract1M x2 ex
+          typ2' <- Pi h p1 t2 <$> abstract1M x2 s2'
+          return (e2, typ2')
+        (Var v@(metaRef -> Just r), Pi h p t2 s2) -> do
+          sol <- solution r
+          case sol of
+            Left l -> do
+              occurs l v typ2
+              unify (metaType v) Type
+              t11  <- freshExistsLV (metaHint v) Type l
+              t12 <- freshExistsLV (metaHint v) Type l
+              solve r $ Pi h p t11 $ abstractNothing t12
+              x2  <- freshForall h t2
+              (x1, t11') <- subtype (return x2) t2 t11
+              (ex, s2')  <- subtype (betaApp e p x1) t12 (instantiate1 (return x2) s2)
+              refineSolved r . Pi h p t11' =<< abstract1M x2 s2'
+              e2    <- etaLam h p t2 <$> abstract1M x2 ex
+              typ2' <- Pi h p t2 <$> abstract1M x2 s2'
+              return (e2, typ2')
+            Right c -> subtype e c typ2
+        (_, Pi h Implicit t2 s2) -> do
+          x2 <- freshForall h t2
+          (e2, s2') <- subtype e typ1 (instantiate1 (return x2) s2)
+          e2'   <- etaLam h Implicit t2 <$> abstract1M x2 e2
+          typ2' <- Pi     h Implicit t2 <$> abstract1M x2 s2'
+          return (e2', typ2')
+        (Pi h Implicit t1 s1, _) -> do
+          v1 <- freshExistsV h t1
+          subtype (betaApp e Implicit v1) (instantiate1 v1 s1) typ2
+        _ | reduce -> do
+          typ1' <- whnf typ1
+          typ2' <- whnf typ2
+          go False e typ1' typ2'
+        _ -> do unify typ1 typ2; return (e, typ2)
