39c3cc1be947435c3bef7d708257207e3d884e9b
Olle Fredriksson <fredriksson.olle@gmail.com>
2016-03-27 18:11:09 +0200

    Add indirection builtins

--- src/Builtin.hs
+++ src/Builtin.hs
@@ -5,7 +5,7 @@ import qualified Data.HashMap.Lazy as HM
 import Data.Maybe
 
 import Syntax
-import Syntax.Abstract
+import Syntax.Abstract as Abstract
 import Util
 
 pattern SizeName <- ((==) "Size" -> True) where SizeName = "Size"
@@ -20,6 +20,18 @@ pattern MaxSize e1 e2 = App (App (Global MaxSizeName) ReEx e1) ReEx e2
 pattern TypeName <- ((==) "Type" -> True) where TypeName = "Type"
 pattern Type sz = App (Global TypeName) IrIm sz
 
+pattern IndArgTypeName <- ((==) "IndArg" -> True) where IndArgTypeName = "IndArg"
+pattern IndArgType sz t = App (App (Global IndArgTypeName) IrIm sz) IrEx t
+
+pattern IndRetTypeName <- ((==) "IndRet" -> True) where IndRetTypeName = "IndRet"
+pattern IndRetType sz t = App (App (Global IndRetTypeName) IrIm sz) IrEx t
+
+pattern IndArgName <- ((==) "indArg" -> True) where IndArgName = "indArg"
+pattern IndArg sz t a = App (App (App (Global IndArgName) ReIm sz) IrIm t) ReEx a
+
+pattern IndRetName <- ((==) "indRet" -> True) where IndRetName = "indRet"
+pattern IndRet sz t a = App (App (App (Global IndRetName) ReIm sz) IrIm t) ReEx a
+
 pointer :: Name
 pointer = "Ptr"
 
@@ -32,10 +44,26 @@ context = HM.fromList
   , (AddSizeName, opaque $ arrow ReEx Size $ arrow ReEx Size Size)
   , (MaxSizeName, opaque $ arrow ReEx Size $ arrow ReEx Size Size)
   , (TypeName, opaque $ arrow IrIm Size $ Type $ Lit 0)
-  , (pointer, dataType (pi_ "size" IrIm Size $ arrow IrEx (Type $ pure "size") $ Type $ Lit 1)
+  , (pointer, dataType (Abstract.pi_ "size" IrIm Size
+                       $ arrow IrEx (Type $ pure "size")
+                       $ Type $ Lit 1)
                        [ ConstrDef ref $ toScope $ fmap B $ arrow ReEx (pure 1)
                                        $ apps (Global pointer) [(IrIm, pure 0), (IrEx, pure 1)]
                        ])
+  , (IndArgTypeName, opaque $ Abstract.pi_ "size" IrIm Size
+                            $ arrow IrEx (Type $ pure "size")
+                            $ Type $ Lit 1)
+  , (IndRetTypeName, opaque $ Abstract.pi_ "size" IrIm Size
+                            $ arrow IrEx (Type $ pure "size")
+                            $ Type $ Lit 1)
+  , (IndRetName, opaque $ Abstract.pi_ "size" ReIm Size
+                        $ Abstract.pi_ "t" IrIm (Type $ pure "size")
+                        $ arrow ReEx (Type $ pure "t")
+                        $ IndRetType (pure "size") (pure "t"))
+  , (IndArgName, opaque $ Abstract.pi_ "size" ReIm Size
+                        $ Abstract.pi_ "t" IrIm (Type $ pure "size")
+                        $ arrow ReEx (Type $ pure "t")
+                        $ IndArgType (pure "size") (pure "t"))
   ]
   where
     cl = fromMaybe (error "Builtin not closed") . closed
