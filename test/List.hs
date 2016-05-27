20d4012d228864ec8f02d7932ab08b9a1536930a
Olle Fredriksson <fredriksson.olle@gmail.com>
2016-05-27 22:04:54 +0200

    Tweak type sigs in List test

--- test/List.hs
+++ test/List.hs
@@ -3,7 +3,7 @@ data List a where
   Nil : List a
   Cons : a -> Ptr (List a) -> List a
 
-deref : forall ~{t}. Ptr t -> t
+deref : forall {n}~{t : Type {n}}. Ptr t -> t
 deref p = case p of Ref t -> t
 
 tail : forall ~{a}. Ptr (List a) -> Ptr (List a)
@@ -11,7 +11,7 @@ tail xs = case deref xs of
   Nil -> Ref Nil
   Cons x xs' -> xs'
 
-map : forall {m}{n}{a : Type {m}}~{b : Type {n}}. (a -> b) -> Ptr (List a) -> Ptr (List b)
+map : forall {m}{n}~{a : Type {m}}~{b : Type {n}}. (a -> b) -> Ptr (List a) -> Ptr (List b)
 map f xs = Ref (case deref xs of
   Nil -> Nil
   Cons x xs' -> Cons (f x) (map f xs'))
