6f246e99ce49408f74192089dae19f8de0884412
Olle Fredriksson <fredriksson.olle@gmail.com>
2016-09-01 09:17:34 +0200

    Update List example

--- test/List.hs
+++ test/List.hs
@@ -1,4 +1,4 @@
-deref : forall {n}~{t : Type {n}}. Ptr t -> t
+deref : forall {n}{t : Type {n}}. Ptr t -> t
 deref p = case p of Ref t -> t
 
 data List {a : Size}(A : Type {a}) where
@@ -9,21 +9,21 @@ data List2 (A : Type) where
   Nil2 : List2 A
   Cons2 : A -> Ptr (List2 A) -> List2 A
 
-tail : forall ~{A : Type}. Ptr (List A) -> Ptr (List A)
+tail : forall {A : Type}. Ptr (List A) -> Ptr (List A)
 tail xs = case deref xs of
-  Nil -> xs
   Cons x xs' -> xs'
+  Nil -> xs
 
 tail2 xs = case deref xs of
-  Nil -> xs
   Cons x xs' -> xs'
+  Nil -> xs
 
-tail3 : forall ~{A}. Ptr (List A) -> Ptr (List A)
+tail3 : forall {A}. Ptr (List A) -> Ptr (List A)
 tail3 xs = case deref xs of
-  Nil -> xs
   Cons x xs' -> xs'
+  Nil -> xs
 
-map : forall {m}{n}~{a : Type {m}}~{b : Type {n}}. (a -> b) -> Ptr (List a) -> Ptr (List b)
+map : forall {m}{n}{a : Type {m}}{b : Type {n}}. (a -> b) -> Ptr (List a) -> Ptr (List b)
 map f xs = Ref (case deref xs of
   Nil -> Nil
   Cons x xs' -> Cons (f x) (map f xs'))
@@ -32,10 +32,14 @@ map2 f xs = Ref (case deref xs of
   Nil -> Nil
   Cons x xs' -> Cons (f x) (map2 f xs'))
 
-sizeof : forall {n}. ~(Type {n}) -> Size
+map3 f xs = case deref xs of
+  Cons x xs' -> Ref (Cons (f x) (map2 f xs'))
+  Nil -> Ref Nil
+
+sizeof : forall {n}. (Type {n}) -> Size
 sizeof {n} _ = n
 
-sizeOfList = printSize (sizeof ~(List (List Size)))
+sizeOfList = printSize (sizeof (List (List Size)))
 
 -- mappedList : Ptr (List Size)
 -- mappedList = map (addSize 1) testList
