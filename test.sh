0e6b3ec39fd2812cf1d1e4cfba8d97e19ae139f3
Olle Fredriksson <fredriksson.olle@gmail.com>
2016-11-05 22:59:36 +0100

    Add some failing tests

--- test.sh
+++ test.sh
@@ -4,7 +4,18 @@ set -e
 set -u
 
 stack install
-for file in tests/*.vix
+
+for file in tests/success/*.vix
 do
   sixten test $file --expected $file-expected
+done
+
+for file in tests/syntax-error/*.vix
+do
+  sixten test $file --expect-syntax-error
+done
+
+for file in tests/type-error/*.vix
+do
+  sixten test $file --expect-type-error
 done
