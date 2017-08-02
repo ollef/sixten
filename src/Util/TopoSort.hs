1922cfe37fbfd614c6c18352fd372aa3338a8e8e
Olle Fredriksson <fredriksson.olle@gmail.com>
2017-08-02 14:04:28 +0200

    Add topoSortWith

--- src/Util/TopoSort.hs
+++ src/Util/TopoSort.hs
@@ -2,6 +2,18 @@ module Util.TopoSort where
 
 import Data.Foldable
 import Data.Graph
+import Data.Hashable
+import qualified Data.HashMap.Lazy as HashMap
+
+topoSortWith
+  :: (Foldable t, Functor t, Foldable t', Hashable name, Ord name)
+  => (a -> name)
+  -> (a -> t' name)
+  -> t a
+  -> [[a]]
+topoSortWith name deps as = (>>= fmap (nameMap HashMap.!)) $ topoSort $ (\a -> (name a, deps a)) <$> as
+  where
+    nameMap = foldl' (\m dat -> HashMap.insertWith (++) (name dat) [dat] m) mempty as
 
 topoSort
   :: (Foldable t, Foldable t', Ord a)
