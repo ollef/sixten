0b5396b78fc4ce4be22482e8fb57dcff04f26d9e
Olle Fredriksson <fredriksson.olle@gmail.com>
2017-09-18 21:19:45 +0200

    TypeOf: Reduce duplication
    The class idea from the TODO file didn't pan out.

--- src/Inference/TypeOf.hs
+++ src/Inference/TypeOf.hs
@@ -13,42 +13,18 @@ import VIX
 typeOfM
   :: AbstractM
   -> VIX AbstractM
-typeOfM expr = do
-  -- logMeta "typeOfM" expr
-  modifyIndent succ
-  t <- case expr of
-    Global v -> do
-      (_, typ) <- definition v
-      return typ
-    Var v -> return $ metaType v
-    Con qc -> qconstructor qc
-    Lit l -> return $ typeOfLiteral l
-    Pi {} -> return Builtin.Type
-    Lam h a t s -> do
-      x <- forall h t
-      resType  <- typeOfM (instantiate1 (pure x) s)
-      abstractedResType <- abstract1M x resType
-      return $ Pi h a t abstractedResType
-    App e1 a e2 -> do
-      e1type <- typeOfM e1
-      e1type' <- whnf e1type
-      case e1type' of
-        Pi _ a' _ resType | a == a' -> return $ instantiate1 e2 resType
-        _ -> throwError $ "typeOfM: expected pi type " ++ show e1type'
-    Let ds s -> do
-      xs <- forMLet ds $ \h _ t -> forall h t
-      typeOfM $ instantiateLet pure xs s
-    Case _ _ retType -> return retType
-    ExternCode _ retType -> return retType
-  modifyIndent pred
-  -- logMeta "typeOfM res" =<< zonk t
-  return t
+typeOfM = typeOfGen abstract1M
 
 typeOf
   :: AbstractM
   -> VIX AbstractM
-typeOf expr = do
-  -- logMeta "typeOf" expr
+typeOf = typeOfGen (\x e -> pure $ abstract1 x e)
+
+typeOfGen
+  :: (MetaA -> AbstractM -> VIX (Scope () Expr MetaA))
+  -> AbstractM
+  -> VIX AbstractM
+typeOfGen abstr expr = do
   modifyIndent succ
   t <- case expr of
     Global v -> do
@@ -60,22 +36,21 @@ typeOf expr = do
     Pi {} -> return Builtin.Type
     Lam h a t s -> do
       x <- forall h t
-      resType <- typeOf (instantiate1 (pure x) s)
-      let abstractedResType = abstract1 x resType
+      resType  <- typeOfGen abstr (instantiate1 (pure x) s)
+      abstractedResType <- abstr x resType
       return $ Pi h a t abstractedResType
     App e1 a e2 -> do
-      e1type <- typeOf e1
+      e1type <- typeOfGen abstr e1
       e1type' <- whnf e1type
       case e1type' of
         Pi _ a' _ resType | a == a' -> return $ instantiate1 e2 resType
-        _ -> throwError $ "typeOf: expected pi type " ++ show e1type'
+        _ -> throwError $ "typeOfGen: expected pi type " ++ show e1type'
     Let ds s -> do
       xs <- forMLet ds $ \h _ t -> forall h t
-      typeOf $ instantiateLet pure xs s
+      typeOfGen abstr $ instantiateLet pure xs s
     Case _ _ retType -> return retType
     ExternCode _ retType -> return retType
   modifyIndent pred
-  -- logMeta "typeOf res" =<< zonk t
   return t
 
 typeOfLiteral
