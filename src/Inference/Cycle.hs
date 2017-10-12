e1701c76713c3060916866869c0ea8a0d9224e93
Olle Fredriksson <fredriksson.olle@gmail.com>
2017-10-12 23:48:01 +0200

    Rework cycle detector
    The previous implementation had a bug which let cyclic constants
    through. This fixes that. See the comments for a motivation of the
    current implementation.

--- src/Inference/Cycle.hs
+++ src/Inference/Cycle.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE MonadComprehensions, OverloadedStrings, ViewPatterns #-}
 module Inference.Cycle where
 
-import Control.Applicative
 import Control.Monad.Except
 import Data.Bitraversable
 import Data.Foldable as Foldable
+import Data.Hashable
+import Data.HashMap.Lazy(HashMap)
 import qualified Data.HashMap.Lazy as HashMap
+import Data.HashSet(HashSet)
+import qualified Data.HashSet as HashSet
 import Data.Monoid
 import qualified Data.Vector as Vector
 import Data.Vector(Vector)
@@ -16,6 +19,7 @@ import Inference.Monad
 import Meta
 import Syntax
 import Syntax.Abstract
+import Util
 import Util.TopoSort
 
 detectTypeRepCycles
@@ -46,46 +50,101 @@ detectTypeRepCycles defs = do
             mempty)
     [] -> return ()
 
+-- We need to detect possible cycles in top-level constants because if not,
+-- we'll accept e.g. the following program:
+--
+-- i = f MkUnit
+-- f MkUnit = i
+--
+-- This would be fine (but loop) in a lazy setting but using CBV and our
+-- compilation scheme where constants are initialised at program startup, this
+-- actually means that i gets some default value like 0. (i is compiled to a
+-- memory cell that's initially zero, then initialized to the result of `f
+-- MkUnit` at program startup, but `f MkUnit` will just read i's memory cell,
+-- which is zero).
+--
+-- Functions are allowed to be mutually recursive; only constants are affected.
+--
+-- The vanilla approach is to restrict the use of definitions in the same
+-- binding group when defining constants.
+--
+-- We additionally allow the use of _functions_ from the same binding group if
+-- they're definitely not immediately _applied_ in the definition of the
+-- constant. The reason we do this is that class instances often end up on
+-- safe-but-not-vanilla forms like `inst = let f = ... inst ... in MkClass f`.
+--
+-- So when is x definitely not immediately applied in the definition of c?
+--
+-- Either x doesn't occur free in c at all, or c is of one of the following
+-- forms:
+--
+-- c = Con as   where x is definitely not applied in as
+-- c = \y. e
+-- c = x
+--
+-- We also peel off any lets at the top-level of all definitions and include
+-- them in the analysis.
 detectDefCycles
   :: Vector (SourceLoc, (MetaA, Definition Expr MetaA, AbstractM))
   -> Infer ()
 detectDefCycles defs = do
-  defExprs <- traverse
-    (bitraverse pure zonk)
-    [(v, e) | (_, (v, Definition _ _ e, _)) <- defs]
-  let locMap = HashMap.fromList $ Vector.toList [(v, loc) | (loc, (v, _, _)) <- defs]
-      recs =
-        [ (v, unguardedOccurrences e)
-        | (v, e) <- defExprs
-        ]
-  case cycles recs of
-    firstCycle:_ -> do
-      let headVar = head firstCycle
-          loc = locMap HashMap.! headVar
-          showVar v = showMeta (pure v :: AbstractM)
-      printedHeadVar <- showVar headVar
-      printedCycle <- mapM showVar $ drop 1 firstCycle ++ [headVar]
-      throwError
-        $ show (explain loc
-          $ Err
-            (Just "Circular definition")
-            ([ "The definition of " <> Leijen.red printedHeadVar <> " is circular."
-            , "It depends on " <> Leijen.dullblue (head printedCycle)
-            ] ++
-            ["which depends " <> Leijen.dullblue v' | v' <- drop 1 printedCycle]
-            )
-            mempty
-            mempty)
-    [] -> return ()
+  (peeledDefExprs, locMap) <- peelLets [(loc, v, e) | (loc, (v, Definition _ _ e, _)) <- Vector.toList defs]
+  forM_ (topoSortWith fst snd peeledDefExprs) $ \scc -> case scc of
+    AcyclicSCC _ -> return ()
+    CyclicSCC defExprs -> do
+      let (functions, constants) = foldMap go defExprs
+            where
+              go (v, Lam {}) = (HashSet.singleton v, mempty)
+              go (v, _) = (mempty, HashSet.singleton v)
+      forM_ defExprs $ \(var, expr) -> case expr of
+        Lam {} -> return ()
+        _ -> do
+          let loc = locMap HashMap.! var
+              showVar v = showMeta (pure v :: AbstractM)
+              constantsInAllOccs = toHashSet expr `HashSet.intersection` constants
+              functionsInImmAppOccs = possiblyImmediatelyAppliedVars expr `HashSet.intersection` functions
+              circularOccs = constantsInAllOccs <> functionsInImmAppOccs
+          unless (HashSet.null circularOccs) $ do
+            printedVar <- showVar var
+            printedOccs <- mapM showVar $ HashSet.toList circularOccs
+            throwError
+              $ show (explain loc
+                $ Err
+                  (Just "Circular definition")
+                  [ "The definition of " <> Leijen.red printedVar <> " is circular."
+                  , "It depends on " <> prettyHumanList "and" (Leijen.dullblue <$> printedOccs) <> " from the same binding group."
+                  ]
+                  mempty
+                  mempty)
 
-unguardedOccurrences
-  :: Expr v
-  -> [v]
-unguardedOccurrences (Let (LetRec ds) (fromScope -> Lam _ _ typ _))
-  = concat ((\(LetBinding _ s t) -> freeVars (unguardedOccurrences $ fromScope s) <|> toList t) <$> Vector.toList ds)
-  <|> freeVars typ
+peelLets
+  :: [(SourceLoc, MetaA, AbstractM)]
+  -> Infer ([(MetaA, AbstractM)], HashMap MetaA SourceLoc)
+peelLets = fmap fold . mapM go
   where
-    freeVars :: Foldable f => f (Var b v) -> [v]
-    freeVars = foldMap $ foldMap pure
-unguardedOccurrences (Lam _ _ t _) = toList t
-unguardedOccurrences e = toList e
+    go
+     :: (SourceLoc, MetaA, Expr MetaA)
+     -> Infer ([(MetaA, Expr MetaA)], HashMap MetaA SourceLoc)
+    go (loc, v, e) = do
+      e' <- zonk e
+      case e' of
+        Let ds scope -> do
+          vs <- forMLet ds $ \h _ t ->
+            forall h Explicit t
+          let inst = instantiateLet pure vs
+          es <- forMLet ds $ \_ s _ ->
+            return $ inst s
+          let ds' = (loc, v, inst scope) : Vector.toList (Vector.zipWith ((,,) loc) vs es)
+          peelLets ds'
+        _ -> return ([(v, e')], HashMap.singleton v loc)
+
+possiblyImmediatelyAppliedVars
+  :: (Eq v, Hashable v)
+  => Expr v
+  -> HashSet v
+possiblyImmediatelyAppliedVars = go
+  where
+    go (Var _) = mempty
+    go Lam {} = mempty
+    go (appsView -> (Con _, xs)) = HashSet.unions [go x | (_, x) <- xs]
+    go e = toHashSet e
