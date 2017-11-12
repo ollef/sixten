fa86ea0e9b99596e85e7fb88013eec67d7a0adeb
theindigamer <theindigamer15@gmail.com>
2017-11-12 16:49:19 -0500

    Simplifies test suite code.
    Also deleted -expected files added earlier as they're not needed now.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,9 +1,9 @@
-import Control.Applicative (liftA)
-import Data.ByteString.Char8 (pack)
-import Data.ByteString.Lazy  (ByteString, fromStrict)
+import Control.Applicative   (liftA)
+import Control.Monad         (join, filterM)
+import Data.List        (isInfixOf, isSuffixOf)
 import Data.Proxy       (Proxy (..))
+import System.Directory (doesDirectoryExist, listDirectory, doesFileExist)
 import System.FilePath  (takeDirectory, FilePath, (</>))
-import System.Process   (readProcess)
 import Test.Tasty
 import Test.Tasty.Golden  (findByExtension)
 import Test.Tasty.Options (IsOption (..), OptionDescription (Option))
@@ -12,14 +12,8 @@ import Test.Tasty.Program (testProgram)
 
 main :: IO ()
 main = do
-  vix <- liftA concat . sequence $ getInput <$>
-    [ TI {arity = Single [], expect = Success}
-    , TI {arity = Single [], expect = SyntaxError}
-    , TI {arity = Single [], expect = TypeError}
-    , TI {arity = Multi ([], []), expect = Success}
-    , TI {arity = Multi ([], []), expect = TypeError}
-    ]
-  defaultMainWithIngredients ings . askOption $ \args -> tests (args) vix
+  input <- testInput
+  defaultMainWithIngredients ings . askOption $ \args -> mkTestGrp args input
     where
       ings = includingOptions [Option (Proxy :: Proxy Args)] :
         defaultIngredients
@@ -32,29 +26,10 @@ instance IsOption Args where
   optionName = return "sixten-args"
   optionHelp = return "Arguments to supply to the sixten binary."
 
-tests :: Args -> [TestInput] -> TestTree
-tests a vix = testGroup "Tests" [endToEndTests a vix]
-
 testRootDir :: FilePath
 testRootDir = "tests"
-
-data InputArity = Single FilePath | Multi (FilePath, [FilePath])
-
-data InputExpect = Success | SyntaxError | TypeError
-
-data TestInput = TI { arity  :: InputArity
-                    , expect :: InputExpect
-                    }
-
-findVixFiles :: TestInput -> IO [FilePath]
-findVixFiles = findByExtension [".vix"] . testDir
-  where
-    testDir z = testRootDir </> (g z ++ h z)
-    g TI {expect = Success}     = "success"
-    g TI {expect = SyntaxError} = "syntax-error"
-    g TI {expect = TypeError}   = "type-error"
-    h TI {arity  = Single _} = ""
-    h TI {arity  = Multi _}  = "-multi"
+testName :: FilePath -> FilePath
+testName = drop (length testRootDir + 1)
 
 groupByKey :: Ord b => (a -> b) -> [a] -> [(b, [a])]
 groupByKey _  [] = []
@@ -62,32 +37,43 @@ groupByKey f (x : xs) = (kx, x : xlike) : groupByKey f rest
   where kx = f x
         (xlike, rest) = span ((==) kx . f) xs
 
-getInput :: TestInput -> IO [TestInput]
-getInput t@(TI {arity = Single _}) = do
-  v <- findVixFiles t
-  return $ fmap (\x -> TI (Single x) (expect t)) v
-getInput t@(TI {arity = Multi _}) = do
-  v <- findVixFiles t
-  return . fmap (\x -> TI (Multi x) (expect t)) $ groupByKey takeDirectory v
-
-callSixten :: [String] -> String -> IO ByteString
-callSixten a b = fromStrict . pack <$> readProcess "sixten" a b
-
-endToEndTests :: Args -> [TestInput] -> TestTree
-endToEndTests (A a) vix = testGroup "End to end tests" $ mkTest <$> vix
+extraFlags :: FilePath -> [FilePath] -> Maybe FilePath -> [String]
+extraFlags dir input expected = input ++ (f dir) ++ (g expected)
   where
-    mkTest t = testProgram (testName t) "sixten" ("test" : args t) Nothing
-    dropTestDirPrefix = drop (length testRootDir + 1)
-    testName TI {arity = Single v}     = dropTestDirPrefix v
-    testName TI {arity = Multi (d, _)} = dropTestDirPrefix d
-    args t = words a ++ flags t ++ files t ++ expected t
-    flags TI {expect = SyntaxError} = ["--expect-syntax-error"]
-    flags TI {expect = TypeError}   = ["--expect-type-error"]
-    flags _                         = []
-    files TI {arity = Single v}     = [v]
-    files TI {arity = Multi (_, v)} = v
-    expected TI {expect = Success, arity = Single v} =
-      ["--expected", v ++ "-expected"]
-    expected TI {expect = Success, arity = Multi (d, _)} =
-      ["--expected", d </> "Main.vix-expected"]
-    expected _ = []
+    f d
+      | "type-error"   `isInfixOf` d = ["--expect-type-error"]
+      | "syntax-error" `isInfixOf` d = ["--expect-syntax-error"]
+      | "success"      `isInfixOf` d = []
+      | otherwise                    = undefined
+    g Nothing  = []
+    g (Just e) = ["--expected", e]
+
+testInput :: IO [(String, [String], Maybe String)]
+testInput = do
+  ds <- join . liftA (filterM doesDirectoryExist) $
+    fmap (testRootDir </>) <$> listDirectory testRootDir
+  liftA concat $ mapM getInfo ds
+    where
+      getInfo dir
+        | "-multi" `isSuffixOf` dir = multi dir
+        | otherwise = single dir
+      single dir = findByExtension [".vix"] dir >>= mapM g
+        where
+          g v = let e = v ++ "-expected" in do
+            b <- doesFileExist e
+            return $ f (v, extraFlags dir [v] $ pick e b)
+      multi dir = do
+        vs <- groupByKey takeDirectory <$> findByExtension [".vix"] dir
+        mapM g vs
+          where
+            g (d, v) = let e = d </> "Main.hs-expected" in do
+              b <- doesFileExist e
+              return $ f (d, extraFlags d v $ pick e b)
+      f (path, x) = (testName path, x, Nothing)
+      pick e b = if b then Just e else Nothing
+
+mkTestGrp :: Args -> [(TestName, [String], Maybe FilePath)] -> TestTree
+mkTestGrp (A a) = testGroup "End to end tests" . fmap mkTest
+  where
+    mkTest (name, xs, stdin) =
+      testProgram name "sixten" ("test" : (words a ++ xs)) stdin
