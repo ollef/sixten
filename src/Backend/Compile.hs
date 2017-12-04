c6fbe82ae0eec1cee3421f6b2c26024a27b45a94
theindigamer <theindigamer15@gmail.com>
2017-12-03 20:34:13 -0500

    Adds searching logic for LLVM binaries.

--- src/Backend/Compile.hs
+++ src/Backend/Compile.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Backend.Compile where
 
+import Control.Exception
 import Control.Monad
 import Data.Foldable
 import Data.Maybe
 import Data.Monoid
+import Data.Version
 import System.FilePath
 import System.Process
 
@@ -20,34 +24,57 @@ data Arguments = Arguments
   , outputFile :: !FilePath
   }
 
+supportedLlvmVersions :: [Version]
+supportedLlvmVersions = makeVersion <$> [[5, 0], [4, 0]]
+
+llvmBinSuffix :: IO String
+llvmBinSuffix = checkLlvmExists trySuffixes
+  where trySuffixes = "" : fmap (('-' :) . showVersion) supportedLlvmVersions
+        checkLlvmExists :: [String] -> IO String
+        checkLlvmExists (suffix : xs) =
+          handle (\(_ :: IOException) -> checkLlvmExists xs) $ do
+          _ <- readProcess ("llvm-config" ++ suffix) ["--version"] ""
+          return suffix
+        checkLlvmExists [] =
+          error "Couldn't find LLVM binaries for supported versions.\n\
+                \Try using the --llvm-config flag."
+
 compile :: Options -> Arguments -> IO ()
 compile opts args = do
-  cLlFiles <- forM (cFiles args) $ compileC opts $ target args
-  linkedLlFile <- linkLlvm (llFiles args ++ toList cLlFiles) $ linkedLlFileName args
-  optLlFile <- optimiseLlvm opts linkedLlFile
-  objFile <- compileLlvm opts (target args) optLlFile
-  assemble opts objFile $ outputFile args
+  suffix <- llvmBinSuffix
+  let opt = "opt" ++ suffix
+  let clang = "clang" ++ suffix
+  let linker = "llvm-link" ++ suffix
+  let compiler = "llc" ++ suffix
+  cLlFiles <- forM (cFiles args) $ compileC clang opts $ target args
+  linkedLlFile <- linkLlvm linker (llFiles args ++ toList cLlFiles)
+    $ linkedLlFileName args
+  optLlFile <- optimiseLlvm opt opts linkedLlFile
+  objFile <- compileLlvm compiler opts (target args) optLlFile
+  assemble clang opts objFile $ outputFile args
 
 optimisationFlags :: Options -> [String]
 optimisationFlags opts = case Options.optimisation opts of
   Nothing -> []
   Just optLevel -> ["-O" <> optLevel]
 
-optimiseLlvm :: Options -> FilePath -> IO FilePath
-optimiseLlvm opts file
+type Binary = FilePath
+
+optimiseLlvm :: Binary -> Options -> FilePath -> IO FilePath
+optimiseLlvm opt opts file
   | isNothing $ Options.optimisation opts = return file
   | otherwise = do
     let optLlFile = replaceExtension file "opt.ll"
-    callProcess "opt" $ optimisationFlags opts ++
+    callProcess opt $ optimisationFlags opts ++
       [ "-S", file
       , "-o", optLlFile
       ]
     return optLlFile
 
-compileC :: Options -> Target -> FilePath -> IO FilePath
-compileC opts tgt cFile = do
+compileC :: Binary -> Options -> Target -> FilePath -> IO FilePath
+compileC clang opts tgt cFile = do
   let output = cFile <> ".ll"
-  callProcess "clang" $ optimisationFlags opts ++
+  callProcess clang $ optimisationFlags opts ++
     [ "-march=" <> Target.architecture tgt
     , "-fvisibility=internal"
     , "-fPIC"
@@ -58,14 +85,14 @@ compileC opts tgt cFile = do
     ]
   return output
 
-linkLlvm :: [FilePath] -> FilePath -> IO FilePath
-linkLlvm [file] _outFile = return file
-linkLlvm files outFile = do
-  callProcess "llvm-link" $ ["-o=" <> outFile, "-S"] ++ files
+linkLlvm :: Binary -> [FilePath] -> FilePath -> IO FilePath
+linkLlvm _ [file] _outFile = return file
+linkLlvm linker files outFile = do
+  callProcess linker $ ["-o=" <> outFile, "-S"] ++ files
   return outFile
 
-compileLlvm :: Options -> Target -> FilePath -> IO FilePath
-compileLlvm opts tgt llFile = do
+compileLlvm :: Binary -> Options -> Target -> FilePath -> IO FilePath
+compileLlvm compiler opts tgt llFile = do
   let flags ft o
         = optimisationFlags opts ++
         [ "-filetype=" <> ft
@@ -77,14 +104,14 @@ compileLlvm opts tgt llFile = do
       asmFile = replaceExtension llFile "s"
       objFile = replaceExtension llFile "o"
   when (isJust $ Options.assemblyDir opts) $
-    callProcess "llc" $ flags "asm" asmFile
-  callProcess "llc" $ flags "obj" objFile
+    callProcess compiler $ flags "asm" asmFile
+  callProcess compiler $ flags "obj" objFile
   return objFile
 
-assemble :: Options -> FilePath -> FilePath -> IO ()
-assemble opts objFile outFile = do
+assemble :: Binary -> Options -> FilePath -> FilePath -> IO ()
+assemble clang opts objFile outFile = do
   ldFlags <- readProcess "pkg-config" ["--libs", "--static", "bdw-gc"] ""
-  callProcess "clang"
+  callProcess clang
     $ concatMap words (lines ldFlags)
     ++ optimisationFlags opts
     ++ [objFile, "-o", outFile]
