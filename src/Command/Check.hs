bece5cfd262750a89dd5b1769052773e6bdc54f7
Brandon Hamilton <brandon.hamilton@gmail.com>
2017-12-06 19:37:05 +0200

    Add 'check' command

---
+++ src/Command/Check.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
+module Command.Check where
+
+import Data.Monoid
+import Data.List.NonEmpty(NonEmpty)
+import Options.Applicative
+import Util
+import System.IO
+import qualified Data.Text.IO as Text
+
+import qualified Backend.Target as Target
+import qualified Processor.Files as Processor
+import qualified Processor.Result as Processor
+
+data Options = Options
+  { inputFiles :: NonEmpty FilePath
+  , verbosity :: Int
+  , logFile :: Maybe FilePath
+  } deriving (Show)
+
+optionsParserInfo :: ParserInfo Options
+optionsParserInfo = info (helper <*> optionsParser)
+  $ fullDesc
+  <> progDesc "Type check a Sixten program"
+  <> header "sixten check"
+
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> nonEmptySome (strArgument
+    $ metavar "FILES..."
+    <> help "Input source FILES"
+    <> action "file"
+    )
+  <*> option auto
+    (long "verbose"
+    <> short 'v'
+    <> metavar "LEVEL"
+    <> help "Set the verbosity level to LEVEL"
+    <> value 0
+    <> completeWith ["0", "10", "20", "30", "40"]
+    )
+  <*> optional (strOption
+    $ long "log-file"
+    <> metavar "FILE"
+    <> help "Write logs to FILE instead of standard output"
+    <> action "file"
+    )
+
+check
+  :: Options
+  -> ([Processor.Error] -> IO ())
+  -> IO ()
+check opts onError = withLogHandle (logFile opts) $ \logHandle -> do
+  procResult <- Processor.checkFiles Processor.Arguments
+        { Processor.sourceFiles = inputFiles opts
+        , Processor.assemblyDir = ""
+        , Processor.target = Target.defaultTarget
+        , Processor.logHandle = logHandle
+        , Processor.verbosity = verbosity opts
+        }
+  case procResult of
+    Processor.Failure errs -> onError errs
+    Processor.Success _ -> Text.putStrLn "✔ Type checking completed succesfully"
+  where
+    withLogHandle Nothing k = k stdout
+    withLogHandle (Just file) k = Util.withFile file WriteMode k
+
+command :: ParserInfo (IO ())
+command = go <$> optionsParserInfo
+  where
+    go opts = check opts (mapM_ Processor.printError)
--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Control.Monad
 import Data.Monoid
 import Options.Applicative
 
+import qualified Command.Check as Check
 import qualified Command.Compile as Compile
 import qualified Command.Run as Run
 import qualified Command.Test as Test
@@ -19,6 +20,7 @@ commands :: Parser (IO ())
 commands = subparser
   $ command "compile" Compile.command
   <> command "run" Run.command
+  <> command "check" Check.command
   <> command "test" Test.command
 
 main :: IO ()
